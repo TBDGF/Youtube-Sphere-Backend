065a93866de16397ae535df5a3f2efcc82f09346
TBDGF <rtbdgf@163.com>
2021-08-16 22:08:56 +0800

    api changed

--- src/main/java/com/privoce/youtube_sphere_backend/controller/AuthingController.java
+++ src/main/java/com/privoce/youtube_sphere_backend/controller/AuthingController.java
@@ -2,11 +2,14 @@ package com.privoce.youtube_sphere_backend.controller;
 
 import cn.authing.core.graphql.GraphQLException;
 import cn.authing.core.types.User;
+import com.privoce.youtube_sphere_backend.entity.SphereUser;
 import com.privoce.youtube_sphere_backend.service.AuthingService;
+import com.privoce.youtube_sphere_backend.service.GraphDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,6 +17,8 @@ import java.util.List;
 public class AuthingController {
     @Autowired
     AuthingService authingService;
+    @Autowired
+    GraphDBService graphDBService;
 
     @GetMapping("/search")
     public List<?> searchByKeyword( String keyword) throws IOException, GraphQLException {
@@ -24,5 +29,15 @@ public class AuthingController {
     @GetMapping("/user")
     public User getAuthingUserById(String userId) throws IOException, GraphQLException {
         return authingService.getUserById(userId);
+    }
+
+    @GetMapping("/connect")
+    public List<User> getFriends(String userId) throws IOException {
+        List<User> result = new ArrayList<>();
+        List<SphereUser> rawUsers = graphDBService.getFriends(userId);
+        for(SphereUser user:rawUsers) {
+            result.add(authingService.getUserById(user.getUserId()));
+        }
+        return result;
     }
 }
