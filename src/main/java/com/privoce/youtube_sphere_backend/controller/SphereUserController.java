42a743fee1f93a118f0a0764f39fac0bf84c4cd0
Curtis Men <curtis_1020@hotmail.com>
2021-09-18 20:53:32 +0800

    reconstruct

--- src/main/java/com/privoce/youtube_sphere_backend/controller/SphereUserController.java
+++ src/main/java/com/privoce/youtube_sphere_backend/controller/SphereUserController.java
@@ -1,10 +1,8 @@
 package com.privoce.youtube_sphere_backend.controller;
 
-import com.privoce.youtube_sphere_backend.entity.Record;
 import com.privoce.youtube_sphere_backend.entity.SphereUser;
 import com.privoce.youtube_sphere_backend.entity.VideoInfo;
 import com.privoce.youtube_sphere_backend.service.GraphDBService;
-import com.privoce.youtube_sphere_backend.service.RecordService;
 import com.privoce.youtube_sphere_backend.service.YoutubeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +17,6 @@ public class SphereUserController {
     GraphDBService graphDBService;
     @Autowired
     YoutubeService youtubeService;
-    @Autowired
-    RecordService recordService;
 
 
     @GetMapping("/connect")
@@ -30,30 +26,7 @@ public class SphereUserController {
 
     @GetMapping("/connect/liked")
     public List<VideoInfo> getFriendsLiked(String userId,Integer num){
-        if (num==null)
-            num=4;
-        Map<String,Integer> map=new HashMap<>();
-        List<SphereUser> list=graphDBService.getFriends(userId);
         List<VideoInfo> videoInfos=new ArrayList<>();
-        for (SphereUser friend:list){
-            List<Record> liked=recordService.getLiked(friend.getUserId());
-            for (Record record:liked){
-                VideoInfo info= youtubeService.getVideoInfo(record.getUrl());
-                Integer index=map.get(info.getVideoId());
-                if (index==null){
-                    info.setUserId(new ArrayList<>());
-                    info.setNickname(new ArrayList<>());
-                    info.getUserId().add(record.getUserId());
-                    info.getNickname().add(graphDBService.getUser(record.getUserId()).getNickname());
-                    videoInfos.add(info);
-                    map.put(info.getVideoId(),map.size());
-                }else{
-                    videoInfos.get(index).getUserId().add(record.getUserId());
-                    videoInfos.get(index).getNickname().add(graphDBService.getUser(record.getUserId()).getNickname());
-                }
-            }
-        }
-        Collections.reverse(videoInfos);
         return videoInfos.size()>num?videoInfos.subList(0,num):videoInfos;
     }
 }
--- src/main/java/com/privoce/youtube_sphere_backend/service/GraphDBService.java
+++ src/main/java/com/privoce/youtube_sphere_backend/service/GraphDBService.java
@@ -1,10 +1,6 @@
 package com.privoce.youtube_sphere_backend.service;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.privoce.youtube_sphere_backend.entity.Record;
 import com.privoce.youtube_sphere_backend.entity.SphereUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,42 +9,17 @@ import java.util.List;
 
 @Service
 public class GraphDBService {
-    @Autowired
-    RecordService recordService;
 
     RestTemplate restTemplate=new RestTemplate();
 
 
     public List<SphereUser> getFriends(String userId) {
         List<SphereUser> list=new ArrayList<>();
-        JSONArray jsonArray = restTemplate.getForObject("https://aws.nicegoodthings.com" + "/user/connect?userId=" + userId + "&depth=0", JSONArray.class);
-        if (jsonArray!=null){
-            for (int i=0;i<jsonArray.size();i++){
-                JSONObject temp= jsonArray.getObject(i,JSONObject.class);
-                list.add(new SphereUser((String) temp.getObject("userId",List.class).get(0),(String) temp.getObject("nickname",List.class).get(0)));
-            }
-            for (SphereUser user:list) {
-                List<String> liked=new ArrayList<>();
-                for (Record record:recordService.getLiked(user.getUserId())){
-                    liked.add(record.getUrl());
-                }
-                user.setLiked(liked);
-            }
-        }
         return list;
     }
 
     public SphereUser getUser(String userId){
         SphereUser user=new SphereUser();
-        JSONObject originObject=restTemplate.getForObject("https://aws.nicegoodthings.com/user?userId="+userId,JSONObject.class);
-        JSONObject fixedObject=originObject.getJSONObject("data").getJSONObject("user");
-        user.setUserId((String) fixedObject.getObject("userId",List.class).get(0));
-        user.setNickname((String) fixedObject.getObject("nickname",List.class).get(0));
-        List<String> liked=new ArrayList<>();
-        for (Record record:recordService.getLiked(user.getUserId())){
-            liked.add(record.getUrl());
-        }
-        user.setLiked(liked);
         return user;
     }
 }
